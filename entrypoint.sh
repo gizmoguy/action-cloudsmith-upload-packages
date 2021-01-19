611874da3d4d3e18b3c40981e84ec99f30ac04c6
Brad Cowie <brad@wand.net.nz>
2021-01-20 09:26:13 +1300

    disable republish

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ CLOUDSMITH_REPO="${2}"
 CLOUDSMITH_USERNAME="${3}"
 export CLOUDSMITH_API_KEY="${4}"
 
-cloudsmith_default_args=(--no-wait-for-sync --republish)
+cloudsmith_default_args=(--error-retry-max 30)
 
 # required to make python 3 work with cloudsmith script
 export LC_ALL=C.UTF-8
